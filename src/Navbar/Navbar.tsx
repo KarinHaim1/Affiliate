27a22440305e79aaa905c061d4aece74d3b82403
KarinHaim1 <haimkarin1@gmail.com>
2025-10-20 14:47:00 +0300

    Update site with Navbar and pages

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Affiliate Compare</h1>
+      <div className="nav-links">
+        <Link to="/">בית</Link>
+        <Link to="/category/clothes">בגדים</Link>
+        <Link to="/category/electronics">אלקטרוניקה</Link>
+        <Link to="/category/home">כלי בית</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar
