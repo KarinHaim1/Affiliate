8289f6abb4dcf5897072b75702c36ec564513a23
KarinHaim1 <haimkarin1@gmail.com>
2025-10-20 15:24:40 +0300

    Updated navbar

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -5,12 +5,14 @@ import './Navbar.css';
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <img src="logo" alt="logo" />
+      <img id='logo' src="logo.jpg" alt="logo" />
       <div className="nav-links">
         <Link to="/">בית</Link>
         <Link to="/category/clothes">בגדים</Link>
         <Link to="/category/electronics">אלקטרוניקה</Link>
         <Link to="/category/home">כלי בית</Link>
+                 <img id='menu' src="menu.png" alt="menu" />
+
       </div>
     </nav>
   );
